242494e8d1604798e773575ae2b9563f95263e4b
SJ <jszymon@gmail.com>
2021-01-05 16:15:27 +0100

    start topological checks

--- py3do/topo.py
+++ py3do/topo.py
@@ -14,8 +14,8 @@ def edge_face_map(m):
 
     Edges are sorted pairs of vertex numbers.  Edge orientation in a
     face is also computed: 0 - ascending, 1 - descending."""
-    edges1 = n.faces[:,0:2]
-    edges2 = n.faces[:,1:3]
-    edges3 = np.hstack([faces[:,2], faces[:,0]])
+    edges1 = m.faces[:,0:2]
+    edges2 = m.faces[:,1:3]
+    edges3 = np.hstack([m.faces[:,2], m.faces[:,0]])
     edges = np.concatenate([edges1, edges2, edges3])
     orientations = (edges[:,0] < edges[:,1])
