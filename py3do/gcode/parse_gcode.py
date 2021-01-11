25468759a9cc174fc775b830c62bf565a7ab6df4
Szymon Jaroszewicz <jszymon@gmail.com>
2021-01-11 01:17:31 +0100

    start G0 and G1

--- py3do/gcode/parse_gcode.py
+++ py3do/gcode/parse_gcode.py
@@ -22,12 +22,17 @@ def parse_gcode(f):
     # state
     slicer = None
     x = y = z = e = None
-    fr = None # feed rate
+    feed_rate = None
     rel_extrude = False
     rel_coord = False
     speed_factor = 1.0
     extrude_factor = 1.0
+    x_offset = 0.0 # offsets to handle G92
+    y_offset = 0.0
+    z_offset = 0.0
+    e_offset = 0.0
     # end state
+    events = []
     with f_ctx as fl:
         for li, l in _numbered_line_reader(fl):
             l = l.strip()
@@ -37,7 +42,7 @@ def parse_gcode(f):
             _split_line = l.split(";", maxsplit=1)
             cmd = _split_line[0].upper()
             cmt = _split_line[1] if len(_split_line) > 1 else None
-            if cmt.startswith("Generated with Cura_SteamEngine"):
+            if cmt is not None and cmt.startswith("Generated with Cura_SteamEngine"):
                 slicer = "Cura"
             if cmd == "":
                 continue
@@ -71,7 +76,43 @@ def parse_gcode(f):
                 if a:
                     extrude_factor = a / 100
             elif cmd == "G92":
-                pass
+                for a in args:
+                    if a[0] == "E":
+                        e_offset = e
+                        e = 0
+                    elif a[0] == "X":
+                        x_offset = e
+                        x = 0
+                    elif a[0] == "Y":
+                        y_offset = e
+                        y = 0
+                    elif a[0] == "Z":
+                        z_offset = e
+                        z = 0
+                    else:
+                        print("Unknown G92 coordinate")
             elif cmd == "G0" or cmd == "G1":
-                pass
-            
+                for a in args:
+                    if a[0] == "X":
+                        ax = float(a[1:])
+                    elif a[0] == "Y":
+                        ay = float(a[1:])
+                    # ...............
+                if rel_coord:
+                    pass
+            else:
+                events.append((li, ["Unhandled Gcode", cmd, args]))
+            # parse Cura comments
+            if slicer == "Cura" and cmt is not None:
+                if cmt.startswith("LAYER:"):
+                    pass
+                if cmt.startswith("TYPE:"):
+                    pass
+                if cmt.startswith("MESH:"):
+                    pass
+
+class GCode:
+    def __init__(self, f):
+        self.parsed = parse_gcode(f)
+    # plot
+    # split into layers
