ff590e740a78d43447487ebb59fc1dfb659dbc2d
Szymon Jaroszewicz <jszymon@gmail.com>
2022-08-24 08:05:47 +0200

    add check for unused vertices to stl_checker

--- examples/stl_checker.py
+++ examples/stl_checker.py
@@ -3,6 +3,7 @@ import sys
 from py3do.io import read_stl
 from py3do.vis import view_pyglet
 from py3do.topo import repeated_face_vertices
+from py3do.topo import unused_vertices
 from py3do.topo import EdgeToFaceMap
 
 m = read_stl(sys.argv[1])
@@ -12,6 +13,13 @@ if len(rep_vert) > 0:
     print(f"Repeated vertices in {len(rep_vert)} faces")
 else:
     print("No repeated vertices")
+
+# can't really happen in an STL, but just in case...
+unu_vert = unused_vertices(m)
+if len(rep_vert) > 0:
+    print(f"{len(unu_vert)} unused vertices")
+else:
+    print("No unused vertices")
 
 efm = EdgeToFaceMap(m)
 print(f"Model is {'' if efm.watertight else 'NOT '}watertight")
