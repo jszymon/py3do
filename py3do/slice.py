fix vertex ordering in sliced faces simplify new normals calculation improve slicing tests

py3do/slice.py
@@ -23,6 +23,8 @@ def slice_horiz_0(m, keep="both", fill=None):
     assert keep in ['both', 'positive', 'negative']
     if fill is None:
         fill = (keep != "both")
+    if fill and keep == "both":
+        raise ValueError("slice: fill unsupported for keep='both'")
     d = m.vertices[:,2] # distances from the plane
     s = np.sign(d) # side of the plane each vertex is on
     fs = s[m.faces]
@@ -145,16 +147,22 @@ def slice_horiz_0(m, keep="both", fill=None):
 
         # prepare data for mapbox_earcut
         # TODO: holes are treated as separate vertices!  fix this!
+        # TODO: triangulation can result in collinear faces
+        # TODO: triangulation may have problems with collinear points
+        nrm = np.array([0.0,0,1 if keep=="negative" else -1])
         for cycle in cycles:
             print(cycle)
             cycle_verts = m_sliced.vertices[cycle][:,:2]
             cycle_faces = mapbox_earcut.triangulate_float64(cycle_verts,
                                                             [len(cycle)])
             cycle_faces = cycle[cycle_faces] # return to original vertex numbering
-            m_sliced.faces = np.vstack([m_sliced.faces,
-                                        cycle_faces.reshape(-1,3)])
-        # TODO: only update normal of new faces
-        m_sliced.normals, _ = normals_cross(m_sliced)
+            cycle_faces = cycle_faces.reshape(-1,3)
+            # reorient faces at bottom
+            if keep == "positive":
+                cycle_faces = cycle_faces[:,::-1]
+            m_sliced.faces = np.vstack([m_sliced.faces, cycle_faces])
+            m_sliced.normals = np.vstack([m_sliced.normals,
+                                          np.tile(nrm, (cycle_faces.shape[0], 1))])
 
     # remove unused vertices
     m_sliced.delete_vertices(unused_vertices(m_sliced))

tests/test_slice.py
@@ -3,6 +3,21 @@ import numpy as np
 from py3do.primitives import cube, uv_sphere
 from py3do.slice import slice_horiz_0
 from py3do import is_isomorphic
+from py3do.topo import EdgeToFaceMap
+
+def _assert_verts_unique(ms):
+    assert np.unique(ms.vertices, axis=0).shape == ms.vertices.shape
+def _assert_faces_on_one_side(ms):
+    vert_sgn = np.sign(ms.vertices[:,2])
+    # assert all faces have all nonnegative and/or nonpositive vertices
+    assert ((vert_sgn[ms.faces] > -0.5).all(axis=1) |
+            (vert_sgn[ms.faces] < 0.5).all(axis=1)).all()
+def _assert_correct_topo(ms, closed=True):
+    efm = EdgeToFaceMap(ms)
+    assert efm.oriented, "Edges not correctly oriented"
+    assert efm.manifold, "Model is not manifold"
+    if closed:
+        assert efm.watertight, "Model is not watertight"
 
 def test_horiz_0_full_positive():
     m=cube()
@@ -24,32 +39,58 @@ def test_horiz_0_cut_cube():
     ms = slice_horiz_0(m, keep="both")
     assert len(ms.vertices) == 8 + 4 + 4 # top/bottom + new
     assert len(ms.faces) == 4 + 4*6 # top/bottom + 4 sides * 6 triangles
-    
+    _assert_verts_unique(ms)
+    _assert_faces_on_one_side(ms)
+    _assert_correct_topo(ms, closed=True)
+def test_horiz_0_cut_cube_positive_open():
+    m = cube()
+    m.vertices[:,2] -= 0.25
+    ms = slice_horiz_0(m, keep="positive", fill=False)
+    assert len(ms.vertices) == 8 + 4 # top/bottom + new on bottom
+    assert len(ms.faces) == 2 + 4*3  # top + 4 sides*3 triangles + bot 6 triangles
+    _assert_verts_unique(ms)
+    _assert_faces_on_one_side(ms)
+    _assert_correct_topo(ms, closed=False)
+def test_horiz_0_cut_cube_positive():
+    m = cube()
+    m.vertices[:,2] -= 0.25
+    ms = slice_horiz_0(m, keep="positive")
+    assert len(ms.vertices) == 8 + 4 # top/bottom + new on bottom
+    assert len(ms.faces) == 2 + 4*3 + 6 # top + 4 sides*3 triangles + bot 6 triangles
+    _assert_verts_unique(ms)
+    _assert_faces_on_one_side(ms)
+    _assert_correct_topo(ms, closed=True)
+def test_horiz_0_cut_cube_negative():
+    m = cube()
+    m.vertices[:,2] -= 0.25
+    ms = slice_horiz_0(m, keep="negative")
+    assert len(ms.vertices) == 8 + 4 # top/bottom + new on bottom
+    assert len(ms.faces) == 2 + 4*3 + 6 # top + 4 sides*3 triangles + bot 6 triangles
+    _assert_verts_unique(ms)
+    _assert_faces_on_one_side(ms)
+    _assert_correct_topo(ms, closed=True)
+def test_horiz_0_cut_cube_pos():
+    m = cube()
+    m.vertices[:,2] -= 0.25
+    ms = slice_horiz_0(m, keep="positive")
+    #assert len(ms.vertices) == 8 + 4 + 4 # top/bottom + new
+    #assert len(ms.faces) == 4 + 4*6 # top/bottom + 4 sides * 6 triangles
+    _assert_verts_unique(ms)
+    _assert_faces_on_one_side(ms)
 
-if __name__ == "__main__":
-    from py3do.vis import view_pyglet
-    m=cube()
-    ms = slice_horiz_0(m)
-    assert is_isomorphic(m, ms) # full model on positive side
-
-    m2 = m.clone()
-    m2.vertices[:,2] += 1
-    m2s = slice_horiz_0(m2, keep="negative")
-    assert len(m2s.vertices) == 0
-    assert len(m2s.faces) == 0
-
-    m3 = m.clone()
-    m3.vertices[:,2] -= 0.25
-    m3s = slice_horiz_0(m3, keep="both")
-    view_pyglet(m3s, marked_vertices = np.arange(len(m3s.vertices)).tolist())
-
-    m4 = uv_sphere(n_u=10)
-    m4.vertices[:,2] -= 0.33
-    m4s = slice_horiz_0(m4, keep="negative")
-    view_pyglet(m4s, marked_vertices = np.arange(len(m4s.vertices)).tolist())
-
-    m5 = uv_sphere(n_u=10)
-    m5.vertices[:,2] *= -1
-    m5.vertices[:,2] -= 0.33
-    m5s = slice_horiz_0(m5, keep="negative")
-    view_pyglet(m5s, marked_vertices = np.arange(len(m5s.vertices)).tolist())
+def test_horiz_0_cut_sphere():
+    for dz in np.linspace(-1,1,20):
+        m = uv_sphere(n_u=10)
+        m.vertices[:,2] -= dz
+        ms = slice_horiz_0(m, keep="positive")
+        _assert_verts_unique(ms)
+        _assert_faces_on_one_side(ms)
+        _assert_correct_topo(ms, closed=True)
+        ms = slice_horiz_0(m, keep="negative")
+        _assert_verts_unique(ms)
+        _assert_faces_on_one_side(ms)
+        _assert_correct_topo(ms, closed=True)
+        ms = slice_horiz_0(m, keep="both")
+        _assert_verts_unique(ms)
+        _assert_faces_on_one_side(ms)
+        _assert_correct_topo(ms, closed=False)