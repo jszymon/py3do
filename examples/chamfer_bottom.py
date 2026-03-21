4a8dfac7b33cc46a8d4e0b088a9031a00b47ed57
Szymon Jaroszewicz <jszymon@gmail.com>
2026-03-21 19:48:22 +0100

    many fixes to bottom chamfering

--- examples/chamfer_bottom.py
+++ examples/chamfer_bottom.py
@@ -1,10 +1,27 @@
+import sys
+import argparse
+
 from py3do import cube, chamfer_bottom
+from py3do.io import read_stl, write_binary_stl
 
 from py3do.vis import view_pyglet
 
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                 description="Chamfer model bottom face.")
+parser.add_argument('stlfile', help='STL file name')
+parser.add_argument('height', help='Height at which chamfer should start')
+parser.add_argument('-d', '--width', help='Width of chamfer at the bottom (default equal toheight)', default="")
+parser.add_argument('-v', '--view', help='Show the model', action='store_true')
+#parser.print_help()
 
+args = parser.parse_args()
+h = float(args.height)
+if args.width == "":
+    d = None
+else:
+    d = float(args.d)
 
-m = cube()
-mc = chamfer_bottom(m, 0.1)
-
-view_pyglet(mc)
+m = read_stl(args.stlfile)
+mc = chamfer_bottom(m, h, d=d)
+if args.view:
+    view_pyglet(mc)
--- tests/test_chamfer.py
+++ tests/test_chamfer.py
@@ -5,7 +5,10 @@ from py3do import cube, chamfer_bottom
 def test_chamfer_cube():
     m = cube()
     m.vertices -= [[0.5, 0.5, 0.5]]
+    bot_verts_orig = m.vertices[m.vertices[:,2] < -0.45]
+    bot_verts_orig = bot_verts_orig[:,:2]
     mc = chamfer_bottom(m, 0.1)
     bot_verts = mc.vertices[mc.vertices[:,2] < -0.45]
     bot_verts = bot_verts[:,:2]
-    assert np.allclose(np.abs(bot_verts), 0.45)
+    dists = np.linalg.norm(bot_verts-bot_verts_orig, axis=1)
+    assert np.allclose(dists, 0.1)
